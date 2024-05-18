ea5c5ae44bd56cc8b73a38caf517aedcbc263886
HaiquanZ <nn764562@gmail.com>
2024-05-18 23:51:58 +0700

    update 18/05

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express');
 const userRouter = require('./api/routes/userRouter');
 const calendarRouter = require('./api/routes/calendarRouter')
 const errorHandler = require('./api/middlewares/handle-error')
-
 const app = express();
+
+//grpc
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync('C:/grpc/helloworld.proto', {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+// const client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
+// client.SayHello({ name: 'world' }, (err, response) => {
+//     if (err) console.error(err);
+//     console.log('Greeting:', response.message);
+//   });
+//
 
 app.use(express.json());
 // app.use('/', (req, res) => {
@@ -12,6 +30,8 @@ app.use(express.json());
 app.use('/', userRouter);
 app.use('/calendar', calendarRouter);
 app.use(errorHandler);
+
+
 
 app.listen(8001, () => {
     console.log('Auth service listening on 8001')
